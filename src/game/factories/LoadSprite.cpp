83131946d3afee33a25ebe07a1e103ee8f10514f
Natalie Martin <natalie@permanent.org>
2024-06-06 17:32:55 -0700

    Remove `LoadSprite` factory
    Since we're going to decompose clipping, scaling, rotation, etc into
    their own optional components, the `Sprite` component is pretty minimal
    and can be default constructed without any issue. This makes the
    `LoadSprite` factory pointless. Get rid of it.

--- src/game/factories/LoadSprite.cpp
+++
@@ -1,9 +0,0 @@
-#include "LoadSprite.h"
-
-Sprite loadSprite(const std::string &resource, Adagio::AbstractTextureManager &textureManager) {
-    Sprite s;
-    s.texture = textureManager.load(resource);
-    s.zIndex = 0;
-    s.position = {0, 0};
-    return s;
-}
--- src/game/factories/LoadSprite.h
+++
@@ -1,9 +0,0 @@
-#ifndef GL_ADAGIO_LOADSPRITE_H
-#define GL_ADAGIO_LOADSPRITE_H
-
-#include "../components/Sprite.h"
-#include "../../graphics/AbstractTextureManager.h"
-
-Sprite loadSprite(const std::string &resource, Adagio::AbstractTextureManager &textureManager);
-
-#endif //GL_ADAGIO_LOADSPRITE_H
