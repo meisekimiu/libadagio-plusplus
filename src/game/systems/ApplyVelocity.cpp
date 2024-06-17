62af3fa2692952a779135c3182ee523aa1ef0970
Natalie Martin <natalie@permanent.org>
2024-06-17 11:41:54 -0500

    Add Velocity Component
    Create a new component called "Velocity" to generalize things that move
    in a specific velocity every frame, and create a system that applies
    that velocity to an entity's position in an isolated, testable, and
    framerate-independant way.
    
    Also remove the speed/velocity calculations from the UserProjectile
    component so we can use the new Velocity component instead.

--- src/game/systems/ApplyVelocity.cpp
+++ src/game/systems/ApplyVelocity.cpp
@@ -5,11 +5,11 @@
 
 void ApplyVelocity(entt::registry &registry, Adagio::GameStats &stats,
                    Adagio::StateMachine *state) {
-  auto view = registry.view<Position, Velocity>();
-  for (auto [entity, pos, velocity] : view.each()) {
-    const double delta =
-        stats.getFrameDelta() * 60; // Speed is in "1 pixel per 1/60 of a frame"
-    pos.position.x += delta * velocity.speed * std::cos(velocity.direction);
-    pos.position.y += delta * velocity.speed * std::sin(velocity.direction);
-  }
+    auto view = registry.view<Position, Velocity>();
+    for (auto [entity, pos, velocity]: view.each()) {
+        // Speed is in "1 pixel per 1/60 of a second"
+        const double delta = stats.getFrameDelta() * 60;
+        pos.position.x += delta * velocity.speed * std::cos(velocity.direction);
+        pos.position.y += delta * velocity.speed * std::sin(velocity.direction);
+    }
 }
